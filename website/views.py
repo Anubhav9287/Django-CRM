fe9f71aa3fd72a5847bd97d5741a226ec90a549f
Anubhav Sharma <anubhav9287@gmail.com>
2024-03-04 00:51:12 -0600

    added records , from database

--- website/views.py
+++ website/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages 
 from .forms import SignUpForm
+from .models import Record
 
 def home(request):
+	records = Record.objects.all()
 	#Check is logging in ?
 	if request.method == 'POST':
 		username = request.POST['username']
@@ -18,7 +20,7 @@ def home(request):
 			messages.success(request,'There was a error. Please login again...')
 			return redirect('home')
 	else:
-		return render(request,'home.html',{})
+		return render(request,'home.html',{'records':records})
 
 def logout_user(request):
 	logout(request)
@@ -41,6 +43,14 @@ def register_user(request):
 		form = SignUpForm()
 		return render(request,'register.html',{'form':form})
 	return render(request,'register.html',{'form':form})
+
+def customer_record(request,pk):
+	if request.user.is_authenticated:
+		customer_record = Record.objects.get(id=pk)
+		return render(request,'record.html',{'customer_record':customer_record})
+	else:
+		messages.success(request,"You must be logged in to view this..")
+		return redirect('home')
 
 
 
